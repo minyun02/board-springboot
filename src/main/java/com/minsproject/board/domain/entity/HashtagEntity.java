b88f110c2568141f78943d5d7d350214db834d67
min <myun02302@gmail.com>
2023-10-02 19:59:41 +0900

    SqlDelete, where 옵션 추가

--- src/main/java/com/minsproject/board/domain/entity/HashtagEntity.java
+++ src/main/java/com/minsproject/board/domain/entity/HashtagEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
@@ -15,6 +17,8 @@ import java.util.Set;
         @Index(columnList = "hashtag_name", unique = true),
         @Index(columnList = "registered_at"),
 })
+@SQLDelete(sql = "UPDATE hashtag SET removed_at = NOW() WHERE id=?")
+@Where(clause = "removed_at is NULL")
 @Entity
 public class HashtagEntity {
 
