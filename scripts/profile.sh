73a75c7f893cb4b50d3e02552d9fbb5f33977efa
min <myun02302@gmail.com>
2023-10-12 01:25:22 +0900

    script modified

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function find_new_profile() {
+function find_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
       if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
@@ -20,9 +20,21 @@ function find_new_profile() {
       echo "${NEW_PROFILE}"
   }
 
-  function find_new_port()
+  function find_old_port()
   {
-      IDLE_PROFILE=$(find_new_profile)
+      IDLE_PROFILE=$(find_profile)
+
+      if [ ${IDLE_PROFILE} == real1 ]
+      then
+        echo "8082"
+      else
+        echo "8081"
+      fi
+}
+
+  function find_port()
+  {
+      IDLE_PROFILE=$(find_profile)
 
       if [ ${IDLE_PROFILE} == real1 ]
       then
@@ -31,35 +43,3 @@ function find_new_profile() {
         echo "8082"
       fi
 }
-
-function find_old_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
-
-      if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-      then
-          CURRENT_PROFILE=real2
-      else
-          CURRENT_PROFILE=$(curl -s http://localhost/profile)
-      fi
-
-      if [ ${CURRENT_PROFILE} == real1 ]
-      then
-        OLD_PROFILE=real2
-      else
-        OLD_PROFILE=real1
-      fi
-
-      echo "${OLD_PROFILE}"
-  }
-
-  function find_old_port()
-  {
-      OLD_PROFILE=$(find_new_profile)
-
-      if [ ${OLD_PROFILE} == real1 ]
-      then
-        echo "8081"
-      else
-        echo "8082"
-      fi
-  }
